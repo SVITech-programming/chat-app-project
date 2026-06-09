d861b3b3f7a2062ef0509ef3d70a083f96bdc3f2
Martin Svitek <martin.svitek92@gmail.com>
2026-06-09 20:53:47 +0200

    first commit

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router";
+import { ChatRoom } from "../pages/ChatRoom";
+import { RoomList } from "../pages/RoomList";
+import { CreateRoom } from "../pages/CreateRoom";
+import { Navbar } from "./Navbar";
+
+export function Dashboard() {
+  return (
+    <section className="chat-app" style={{ color: "white" }}>
+      <Navbar />
+      <Routes>
+        <Route index element={<ChatRoom />} />
+        <Route path="/rooms" element={<RoomList />} />
+        <Route path="/create-room" element={<CreateRoom />} />
+      </Routes>
+    </section>
+  );
+}
